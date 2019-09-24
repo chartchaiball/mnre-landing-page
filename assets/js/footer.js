f5888599fe5ff536b79c6ccee3947dbab15398be
chartchai <chartchai.ball@gmail.com>
2019-09-25 00:41:46 +0700

    update css and header and footer

---
+++ assets/js/footer.js
@@ -0,0 +1,81 @@
+var footerTxt = `<div class="container">
+<div class="row">
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="single-footer-widget">
+            <div class="logo">
+                <a href="#"><img src="assets/img/white-logo.png" alt="image"></a>
+                <p>Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan.</p>
+            </div>
+
+            <ul class="social">
+                <li><a href="https://www.facebook.com/mnreTH/" target="_blank"><i class="flaticon-facebook-letter-logo"></i></a></li>
+                <li><a href="https://twitter.com/PR_MNRE" target="_blank"><i class="flaticon-twitter-black-shape"></i></a></li>
+                <li><a href="https://www.youtube.com/channel/UCJtYzDvnAudN-HrHmpIYAAA" target="_blank"><i class="flaticon-youtube"></i></a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="single-footer-widget">
+            <h3>Services</h3>
+
+            <ul class="footer-services-list">
+                <li><a href="#">Product Engineering</a></li>
+                <li><a href="#">UX/UI Design</a></li>
+                <li><a href="#">Big Data Analysis</a></li>
+                <li><a href="#">Desktop Applications</a></li>
+                <li><a href="#">Mobile Applications</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="single-footer-widget">
+            <h3>Quick Links</h3>
+
+            <ul class="quick-links-list">
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Support</a></li>
+                <li><a href="#">Applications</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="single-footer-widget">
+            <h3>Contacts</h3>
+
+            <ul class="footer-contact-list">
+                <li><span>Address:</span> 92 ซอยพหลโยธิน 7 ถนนพหลโยธิน แขวงพญาไท เขตพญาไท กรุงเทพมหานคร 10400</li>
+                <li><span>Email:</span> <a href="#">webmaster@mnre.go.th</a></li>
+                <li><span>Phone:</span> <a href="#">0 2278 8500</a></li>
+                <li><span>Fax:</span> <a href="#">+44 785 4578964</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+</div>
+<div class="copyright-area">
+<div class="container">
+    <div class="row align-items-center">
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <p>Copyright 2019 <a href="#">Evolta</a>. All Rights Reserved.</p>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <ul>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="circle-map"><img src="assets/img/circle-map.png" alt="image"></div>`;
+
+var footer = document.getElementById("site-footerTxt");
+footer.innerHTML = footerTxt;
+console.log("load");
---
+++ assets/js/header.js
@@ -0,0 +1,120 @@
+var headerTxt = `<div class="top-header">
+<div class="container-fluid">
+    <div class="row align-items-center">
+        <div class="col-lg-6 col-md-6">
+            <p><span>Address:</span> <i class="flaticon-placeholder"></i> 92 ซอยพหลโยธิน 7 ถนนพหลโยธิน แขวงพญาไท เขตพญาไท กรุงเทพมหานคร 10400</p>
+        </div>
+
+        <div class="col-lg-6 col-md-6">
+            <ul>
+                <li><span>Contact Sales:</span> <i class="flaticon-phone-call"></i> <a href="tel:0 2278 8500"> 0 2278 8500</a></li>
+                <li><span>Contact Email:</span> <i class="flaticon-message-closed-envelope"></i> <a href="mailto:evolta@gmail.com"> webmaster@mnre.go.th</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="navbar-area">
+<div class="evolta-responsive-nav">
+    <div class="container">
+            <div class="header">
+            <a href="index.html">
+                    <img src="assets/img/logo_mnre_small.png" alt="logo" height="50px">
+                    <div class="title">กระทรวงทรัพยากรธรรมชาติและสิ่งแวดล้อม</div>
+                    <div class="sub-title">Ministry of Natural Resources and Environment</div>
+                    </a>
+            </div>
+        <div class="evolta-responsive-menu">
+        </div>
+    </div>
+</div>
+
+<div class="evolta-nav">
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-md navbar-light">
+            <a class="navbar-brand" href="index.html">
+                    <img src="assets/img/logo_mnre_small.png" alt="logo" style="max-height: 70px">
+            </a>
+
+            <div class="logo-text">
+                <div class="logo-title">กระทรวงทรัพยากรธรรมชาติและสิ่งแวดล้อม</div>
+                <div class="logo-sub-title">Ministry of Natural Resources and Environment</div>
+            </div>
+
+            <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent" >
+                <ul class="navbar-nav">
+                        <li class="nav-item"><a href="index.html" class="nav-link">หน้าแรก</a></li>
+              
+                    <li class="nav-item"><a href="aboutproject.html" class="nav-link">เกี่ยวกับโครงการ</a></li>
+
+                    <li class="nav-item"><a href="#" class="nav-link">Metadata <i class="fas fa-chevron-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li class="nav-item"><a href="about.html" class="nav-link">About Us</a></li>
+
+                            <li class="nav-item"><a href="features.html" class="nav-link">Features</a></li>
+
+                            <li class="nav-item"><a href="team.html" class="nav-link">Team</a></li>
+
+                            <li class="nav-item"><a href="#" class="nav-link">Services</a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
+
+                                    <li class="nav-item"><a href="single-services.html" class="nav-link">Services Details</a></li>
+                                </ul>
+                            </li>
+
+                            <li class="nav-item"><a href="#" class="nav-link">Blog</a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item"><a href="blog-1.html" class="nav-link">Blog Grid</a></li>
+
+                                    <li class="nav-item"><a href="blog-2.html" class="nav-link">Blog Right Sidebar</a></li>
+
+                                    <li class="nav-item"><a href="single-blog.html" class="nav-link">Blog Details</a></li>
+                                </ul>
+                            </li>
+
+                            <li class="nav-item"><a href="error-404.html" class="nav-link">404 Error</a></li>
+
+                            <li class="nav-item"><a href="coming-soon.html" class="nav-link">Coming Soon</a></li>
+
+                            <li class="nav-item"><a href="faq.html" class="nav-link">FAQ</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item"><a href="#" class="nav-link">Web Servies <i class="fas fa-chevron-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li class="nav-item"><a href="resources.html" class="nav-link">WEb SERVICES</a></li>
+
+                            <li class="nav-item"><a href="single-resources.html" class="nav-link">Resources Details</a></li>
+                        </ul>
+                    </li>
+
+
+                    <li class="nav-item"><a href="contact.html" class="nav-link">ติดต่อเรา</a></li>
+                </ul>
+
+                <!-- <div class="others-options">
+                    <div class="option-item"><i class="search-btn flaticon-search"></i>
+                        <i class="close-btn fas fa-times"></i>
+                        
+                        <div class="search-overlay search-popup">
+                            <div class='search-box'>
+                                <form class="search-form">
+                                    <input class="search-input" name="search" placeholder="Search" type="text">
+
+                                    <button class="search-button" type="submit"><i class="fas fa-search"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+            </div>
+        </nav>
+    </div>
+</div>
+</div>`;
+
+var header = document.getElementById("site-header");
+header.innerHTML = headerTxt;
+console.log("load");
